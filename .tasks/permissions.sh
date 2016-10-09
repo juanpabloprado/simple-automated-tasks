2c9c296821f3007b11eae59b5a659da3623fb39d
juanpabloprado <juan@juanpabloprado.com>
2016-10-09 15:27:57 -0500

    More accurate permissions

--- .tasks/permissions.sh
+++ .tasks/permissions.sh
@@ -4,9 +4,10 @@
 source /home/pablo/.tasks/sites.sh
 
 cd "$ROOT/html"
-# Files
-find . -type f -exec chmod 644 {} +
-# Directories
-find . -type d -exec chmod 755 {} +
-# wp-config.php
-chmod 600 wp-config.php
+
+find . -type d -exec chmod g+s {} \;
+
+chmod g+w wp-content
+
+chmod -R g+w wp-content/themes
+chmod -R g+w wp-content/plugins
